b006b2c7453c8d5e9a9aa3d814a4b21b1c109451
Thinkit-lab <59313880+Thinkit-lab@users.noreply.github.com>
2019-12-30 11:53:04 -0500

    Gantry 5 Framework

--- Extension/InitRuntimeInterface.php
+++ Extension/InitRuntimeInterface.php
@@ -1,24 +1,11 @@
 <?php
 
-/*
- * This file is part of Twig.
- *
- * (c) Fabien Potencier
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Twig\Extension;
 
-/**
- * Enables usage of the deprecated Twig_Extension::initRuntime() method.
- *
- * Explicitly implement this interface if you really need to implement the
- * deprecated initRuntime() method in your extensions.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- */
-interface Twig_Extension_InitRuntimeInterface
-{
+class_exists('Twig_Extension_InitRuntimeInterface');
+
+if (\false) {
+    interface InitRuntimeInterface extends \Twig_Extension_InitRuntimeInterface
+    {
+    }
 }
-
-class_alias('Twig_Extension_InitRuntimeInterface', 'Twig\Extension\InitRuntimeInterface', false);
